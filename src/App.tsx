8a5b304b08644dd076a0da43bc4d52de3432b1a9
Gaziz_Tuleukulov <gaziz.tuleukulov@winfinity.live>
2023-02-26 20:38:28 +0300

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { Suspense, useContext, useState } from "react";
+import { Link, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { Counter } from "./components/Counter/Counter";
-import "./index.scss";
+import "./styles/index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/UseTheme";
 
 export const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
-            test
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>0 страница</Link>
+            <Suspense fallback={<div>...loading</div>}>
+                <Routes>
+                    <Route path={"/about"} element={<AboutPageAsync />} />
+                    <Route path={"/"} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
             <Counter />
         </div>
     );
---
+++ src/theme/ThemeProvaider.tsx
@@ -0,0 +1,24 @@
+import { FC, useMemo, useState } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+
+const defaultState =
+    (window.localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) ||
+    Theme.LIGHT;
+
+export const ThemeProvider: FC = ({ children }) => {
+    const [theme, setTheme] = useState<Theme>(defaultState);
+
+    const defaultProps = useMemo(
+        () => ({
+            theme: theme,
+            setTheme: setTheme,
+        }),
+        [theme, setTheme],
+    );
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
